53b105abd5cdf37a8b01796269d13bc95b9fce6d
jimin <jjimini98@naver.com>
2021-04-17 21:03:46 +0900

    finished Lecture5 p.8

---
+++ C++Midterm/Lecture5.cpp
@@ -0,0 +1,47 @@
+// p.6 포인터 예제 
+
+//#include <iostream>
+//using namespace std;
+//
+//int main(void){
+//	char data[4] = "abc";
+//	char* p1 = data; //포인터 변수 선언  
+//	char* p2; //포인터 변수 선언 
+//
+//	p2 = &data[0];
+//	cout << "p1은 " << p1 << ", p2는 " << p2 <<endl;
+//	cout << "*p1은 "<<*p1 << ", *p2는" << *p2 << endl; 
+//
+//}
+
+
+//p.7 포인터 연습
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//	int a = 1, *p = &a;
+//
+//	cout << "p = " << p << ", p+1 = " << p + 1 << endl;
+//	
+//	system("pause"); 
+//}
+
+//p.8 포인터 연습
+//#include <iostream>
+//using namespace std;
+//
+//int main(void) {
+//	int a, * b, ** c;
+//	a = 7;
+//	b = &a;
+//	c = &b;
+//
+//	cout << "a= " << a << ", *b= " << *b << ", **c = " << **c << endl;
+//	cout << "b = " << b << ", *c = " << *c << ", &b = " << &b << ", c= " << c << endl;
+//	cout << "*(&a) = " << *(&a) << ", *(b) = " << *(b) << ", *(&b) = " << *(&b) << endl;
+//	cout << "*(c) = " << *(c) << ", *(&c) = " << *(&c) << endl;
+//
+//}
+
+
